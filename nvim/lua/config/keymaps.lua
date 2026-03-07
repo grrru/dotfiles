94fbd756ef9e9d80c40ca1f4c02d254684d998e3
grrru <gruns0989@gmail.com>
2026-03-07 23:12:55 +0900

    delete keymap for exiting terminal mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,5 +4,3 @@
 
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode" })
-vim.keymap.set("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
-vim.keymap.set("t", "kj", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
