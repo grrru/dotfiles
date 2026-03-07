37b4d117762106623b8a012db6d5d5cc890e4941
grrru <gruns0989@gmail.com>
2026-03-08 01:07:11 +0900

    add keymap for window size control

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,5 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
-vim.keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode" })
+local map = vim.keymap.set
+
+map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+map("i", "kj", "<Esc>", { desc = "Exit insert mode" })
+
+map("n", "<A-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<A-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<A-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<A-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
