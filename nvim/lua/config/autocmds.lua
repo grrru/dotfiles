58dca676666ea36284987534aa33a6ada262694b
grrru <gruns0989@gmail.com>
2026-03-14 16:44:34 +0900

    preserve view when switching buffers

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Preserve view when switching buffers
+vim.api.nvim_create_autocmd("BufLeave", {
+  callback = function()
+    vim.b._saved_view = vim.fn.winsaveview()
+  end,
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    local saved = vim.b._saved_view
+    if saved then
+      vim.schedule(function()
+        vim.fn.winrestview(saved)
+      end)
+    end
+  end,
+})
