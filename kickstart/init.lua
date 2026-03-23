382632a620ef3d0884490ca688410cfaf6594d96
grrru <gruns0989@gmail.com>
2026-03-23 18:34:45 +0900

    add sidekick.nvim in kickstart

--- kickstart/init.lua
+++ kickstart/init.lua
@@ -255,6 +255,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
+        { '<leader>a', group = '[A]I' },
         { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
--- kickstart/lua/custom/plugins/ai.lua
+++ kickstart/lua/custom/plugins/ai.lua
@@ -1,7 +1,52 @@
 return {
-  -- Add your own plugins here, for example:
   {
     'folke/sidekick.nvim',
+    keys = {
+      {
+        '<c-.>',
+        function() require('sidekick.cli').toggle() end,
+        desc = 'Sidekick Toggle',
+        mode = { 'n', 't', 'i', 'x' },
+      },
+      {
+        '<leader>aa',
+        function() require('sidekick.cli').toggle() end,
+        desc = 'Sidekick Toggle CLI',
+      },
+      {
+        '<leader>as',
+        function() require('sidekick.cli').select() end,
+        desc = 'Sidekick Select CLI',
+      },
+      {
+        '<leader>ad',
+        function() require('sidekick.cli').close() end,
+        desc = 'Sidekick Detach CLI Session',
+      },
+      {
+        '<leader>at',
+        function() require('sidekick.cli').send { msg = '{this}' } end,
+        mode = { 'n', 'x' },
+        desc = 'Sidekick Send This',
+      },
+      {
+        '<leader>af',
+        function() require('sidekick.cli').send { msg = '{file}' } end,
+        desc = 'Sidekick Send File',
+      },
+      {
+        '<leader>av',
+        function() require('sidekick.cli').send { msg = '{selection}' } end,
+        mode = { 'x' },
+        desc = 'Sidekick Send Visual Selection',
+      },
+      {
+        '<leader>ap',
+        function() require('sidekick.cli').prompt() end,
+        mode = { 'n', 'x' },
+        desc = 'Sidekick Select Prompt',
+      },
+    },
     opts = {},
   },
 }
