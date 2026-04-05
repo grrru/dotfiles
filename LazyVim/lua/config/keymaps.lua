2bc17d9a974aece4ee962c4b2b1dc7a54a8fe3bd
grrru <gruns0989@gmail.com>
2026-04-05 15:21:03 +0900

    remove 'Exit insert mode' & add Move buffer keymaps

--- LazyVim/lua/config/keymaps.lua
+++ LazyVim/lua/config/keymaps.lua
@@ -4,13 +4,13 @@
 
 local map = vim.keymap.set
 
-map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
-map("i", "kj", "<Esc>", { desc = "Exit insert mode" })
-
 map("n", "<A-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 map("n", "<A-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 map("n", "<A-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 map("n", "<A-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+map("n", "<leader>bh", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
+map("n", "<leader>bl", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })
 
 vim.api.nvim_create_user_command("WhichKeyRepair", function()
   local ok, triggers = pcall(require, "which-key.triggers")
