cd9ebef016ba1d60bbf702647652e6486bc2ce26
grrru <gruns0989@gmail.com>
2026-04-18 23:42:07 +0900

    move dropbar plugin to editor.lua

--- gruvim/lua/plugins/editor.lua
+++ gruvim/lua/plugins/editor.lua
@@ -463,4 +463,19 @@ return {
       },
     },
   },
+
+  -- Dropbar (winbar breadcrumb)
+  {
+    "Bekaboo/dropbar.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+      require("dropbar").setup()
+      local api = require("dropbar.api")
+      vim.keymap.set("n", "<leader>;", api.pick, { desc = "Pick symbols in winbar" })
+      vim.keymap.set("n", "[;", api.goto_context_start, { desc = "Go to start of current context" })
+      vim.keymap.set("n", "];", api.select_next_context, { desc = "Select next context" })
+    end,
+  },
 }
