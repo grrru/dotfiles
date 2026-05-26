5e3beaaf1437887f397edf48a0b87ae0adea866e
grrru <gruns0989@gmail.com>
2026-05-26 15:24:45 +0900

    Add tmux pane shrink shortcut

--- tmux/scripts/three-pane-layout.sh
+++ tmux/scripts/three-pane-layout.sh
@@ -2,6 +2,14 @@
 
 set -eu
 
+bottom_percent=${1:-30}
+case "$bottom_percent" in
+  ''|*[!0-9]*)
+    tmux display-message 'three-pane-layout bottom pane percent must be a number'
+    exit 0
+    ;;
+esac
+
 window_panes=$(tmux display-message -p '#{window_panes}')
 if [ "$window_panes" -ne 3 ]; then
   tmux display-message 'M-3 layout requires exactly 3 panes'
@@ -25,7 +33,7 @@ trap cleanup EXIT INT TERM
 tmp_window=$(tmux new-window -d -P -F '#{window_id}' -t "$session_id:" -n "$tmp_name")
 tmp_first_pane=$(tmux list-panes -t "$tmp_window" -F '#{pane_id}')
 tmux split-window -d -h -p 32 -t "$tmp_first_pane" >/dev/null
-tmux split-window -d -v -p 30 -t "$tmp_first_pane" >/dev/null
+tmux split-window -d -v -p "$bottom_percent" -t "$tmp_first_pane" >/dev/null
 
 layout=$(tmux display-message -p -t "$tmp_window" '#{window_layout}')
 tmux select-layout -t "$window_id" "$layout" >/dev/null
